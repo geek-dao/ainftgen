ab5180faa3f954d8971a8db4d4a3da363500561b
geek-dao <mr7s1990@outlook.com>
2023-03-04 22:19:07 +0800

    feat: test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useState } from 'react'
 import { useAccount } from 'wagmi'
-import { getImage, getImageFunc } from './api'
+import { getImage } from './api'
 import { Account } from './components'
 
 export function App() {
@@ -8,15 +9,12 @@ export function App() {
   // const res = getImage({ prompt: 'A chinese girl'})
   let content1 = { url: '' }
   let content2 = { url: '' }
-  const res1 = () => {
-    getImageFunc({ prompt: 'A chinese girl'}).then((item: any) => {
-      content1 = item[0]
-    })
-    return
-  }
+  const [imageRes,setImageRes] = useState('')
+
   const res2 = () => {
     getImage({ prompt: 'A chinese girl'}).then(item => {
       content2 = item[0]
+      setImageRes(item[0])
     })
     // console.log('content2', content2)
     return
@@ -27,8 +25,8 @@ export function App() {
 
       <ConnectButton />
       {isConnected && <Account />}
-      <button onClick={res1}>获取图片1</button>
       <button onClick={res2}>获取图片2</button>
+      <img src={imageRes} />
     </>
   )
 }
--- src/api/index.ts
+++ src/api/index.ts
@@ -6,29 +6,11 @@ interface Params {
   size?: CreateImageRequestSizeEnum
 }
 
-export async function getImage ({ prompt, n = 1, size = '1024x1024' }: Params) {
-  const params = {
-    'prompt': prompt,
-    'n': n,
-    'size': size
-  }
-  const response = await fetch('https://api.openai.com/v1/images/generations', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + String('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')
-    },
-    body: JSON.stringify(params)
-  })
-  const data = await response.json()
-  return data ? data.data : ''
-}
 
-export async function getImageFunc ({ prompt, n = 1, size = '1024x1024' }: Params) {
+export async function getImage ({ prompt, n = 1, size = '1024x1024' }: Params) {
   const configuration = new Configuration({
     apiKey: String(import.meta.env.VITE_PRIVATE_KEY),
   });
-  console.log(import.meta.env.PRIVATE_KEY);
   const openai = new OpenAIApi(configuration);
   const params = {
     'prompt': prompt,
@@ -37,5 +19,5 @@ export async function getImageFunc ({ prompt, n = 1, size = '1024x1024' }: Param
   }
   const response = await openai.createImage(params)
   // const response = await openai.createImage(params, { headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36' }})
-  return response ? response.data : ''
+  return response?.data || ''
 }
